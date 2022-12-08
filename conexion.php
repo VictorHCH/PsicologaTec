ff437dcf3b38220dc57bdc711ee0f2327b49a9ba
Victor Hugo Covarrubias Hernández <82013957+VictorHCH@users.noreply.github.com>
2022-12-08 12:55:25 -0700

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,7 +1,7 @@
 <?php
     //Se realiza la conexion a la BD
     // $conexion = new mysqli('localhost','root','','piscotec');``
-    $conexion = new mysqli('localhost:3307','root','','piscotec'); 
+    $conexion = new mysqli('localhost','root','','piscotec'); 
     // conexion de mi lap
     
 ?>  
